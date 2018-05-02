4629f329f83d1cebc4b07363b4e9fe234a5a9e07
lidan <dan.li@vdin.net>
2018-05-02 14:26:08 +0800

    列表动态加载更多

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,9 @@ const app = getApp()
 
 Page({
   data: {
-    contentList: []
+    contentList: [],
+    lastId: 0,
+    bottomText: "加载更多"
   },
   goToDetail: function(e)
   {
@@ -13,21 +15,48 @@ Page({
     })
   },
 
-  onLoad: function () {
+  loadData(lastId)
+  {
     var that = this;
+    var limit = 2;
     wx.request({
       url: 'http://localhost:801/weicms/index.php?s=/addon/Cms/Cms/getList',
-      data: {},
+      data: {lastId: lastId, limit: limit},
       header: {
         'Content-Type': 'application/json'
       },
-      success: function(res) {
+      success: function (res) {
+        if(!res.data)
+        {
+          that.setData({
+            bottomText: '没有更多数据了'
+          })
+          return false;
+        }
+        var len = res.data.length;
+        var dataArr = that.data.contentList;
+        var newData = dataArr.concat(res.data);
+
         that.setData({
-          contentList: res.data
+          lastId: res.data[len - 1].id,
+          contentList: newData
         })
       }
     })
   },
+
+  onLoad: function () {
+    var that = this;
+    
+    this.loadData(0)
+  },
+
+  loadMore: function(e)
+  {
+    var id = e.currentTarget.dataset.lastid;
+    // console.log(e);
+    this.loadData(id);
+  }
   // getUserInfo: function(e) {
   //   console.log(e)
   //   app.globalData.userInfo = e.detail.userInfo

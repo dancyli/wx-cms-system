e7fb2b307637a5d012f4d521bc7a9203396c1585
lidan <dan.li@vdin.net>
2018-05-04 17:49:00 +0800

    首页和用户反馈页面，及反馈数据集成

--- pages/form/form.js
+++ pages/form/form.js
@@ -6,7 +6,69 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    date: "请选择",
+    array: ['美国', '中国', '巴西', '日本'],
+    index: 0,
+    score: 0,
+    isDev: false
+  },
+
+  bindPickerChange: function(e)
+  {
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindDateChange: function(e)
+  {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+
+  sliderChange: function(e)
+  {
+    console.log(e.detail.value)
+    this.setData({
+      score: e.detail.value
+    })
+  },
+
+  switchChange: function(e)
+  {
+    console.log(e.detail.value)
+    this.setData({
+      isDev: e.detail.value
+    })
+  },
+
+  formSubmit: function(e)
+  {
+    var formData = e.detail.value;
+    var that = this;
+
+    formData.area = this.data.index;
+    formData.date = this.data.date;
+    formData.score = this.data.score;
+    formData.is_dev = this.data.isDev;
+
+    console.log(formData)
+
+    wx.request({
+      url: 'http://localhost:801/weicms/index.php?s=/addon/Feedback/Feedback/addFeedback',
+      method: "post",
+      data: formData,
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        wx.showToast({
+          title: '反馈成功，谢谢您的反馈！',
+          icon: 'success',
+          duration: 1500
+        })
+      }
+    })
   },
 
   /**
@@ -16,52 +78,4 @@ Page({
     
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
 })

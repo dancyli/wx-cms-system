08ac257ed123d60751a1d7d5a7a0229d16415280
lidan <dan.li@vdin.net>
2018-05-07 17:24:56 +0800

    小程序选择图片接口，后台暂无

--- pages/create/create.js
+++ pages/create/create.js
@@ -16,52 +16,29 @@ Page({
     
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  chooseImage: function()
+  {
+    wx.chooseImage({
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths;
+        console.log(tempFilePaths)
+        // wx.uploadFile({
+        //   url: 'https://example.weixin.qq.com/upload', //仅为示例，非真实的接口地址
+        //   filePath: tempFilePaths[0],
+        //   name: 'file',
+        //   formData: {
+        //     'user': 'test'
+        //   },
+        //   success: function (res) {
+        //     var data = res.data
+        //     //do something
+        //   }
+        // })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  formSubmit: function(e)
+  {
+    console.log(e.detail.value);
   }
 })
